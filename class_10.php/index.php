5f9a10d9ca758e13a51688051055d03fd65534d8
nurnaby <sohag.biit@gmail.com>
2022-04-13 12:55:37 +0600

    date formet function

---
+++ class_10.php/index.php
@@ -0,0 +1,17 @@
+<?php
+// $colors = ["red","green","blue","yeallow","brown"];
+
+// $newArray = array_slice($colors,2,3);
+// echo"<>";
+// print_r($newArray);
+
+ echo "hello";
+
+ $fruits = array ("apple", "orange", array ("pear", "mango"),  "banana");
+
+ Echo count($fruits);
+
+
+
+
+?>
---
+++ class_11/index.php
@@ -0,0 +1,26 @@
+<?php
+// datt function day name
+echo "this day name".date('D')."<br>";
+echo "this day name".date('l')."<br>";
+echo "this day name".date('N')."<br>";
+echo "this day name".date('N')."<br>";
+echo "this day name".date('w')."<br><br>";
+//month name show
+echo "this month is ".date("F")."<br><br>";
+
+//date full format 
+echo "this day ful format".date('d/m/y')."<br>";
+echo "this day ful format".date('j/M/Y')."<br>";
+echo "this day ful format".date('jS/M/Y')."<br>";
+
+
+echo date('z')."<br>";
+echo date('L');
+echo date('w');
+
+
+
+
+
+
+?>
